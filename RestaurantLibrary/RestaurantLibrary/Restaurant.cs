f184be7333e969427599f0a9c7b00531a006a6a1
Diana Grzelak <diana-teodora.grzelak@sdworx.com>
2023-12-18 14:23:04 +0100

    Revert "prepare the Restaurant class"
    This reverts commit b9814d511b6755dce8f299b8ea274a7e6c6b9072.

--- RestaurantLibrary/RestaurantLibrary/Restaurant.cs
+++ RestaurantLibrary/RestaurantLibrary/Restaurant.cs
@@ -1,37 +1,15 @@
 ﻿namespace RestaurantLibrary
 {
-    public enum Cuisine
-    {
-        Chinese,
-        Japanese,
-        Korean,
-        Thai,
-        Italian,
-        Indian,
-        Mexican,
-        Polish,
-        Lebanese,
-        Romanian,
-        Greek,
-        Turkish,
-        Other
-    }
     public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string ZipCode { get; set; }
-        public Cuisine Cuisine { get; set; }
+        public string Cuisine { get; set; }
 
-
-        public Restaurant(int id, string name, string zipCode, Cuisine cuisine)
+        public List<Restaurant> GetDetails(FileManager<Restaurant> fileManager)
         {
-            Id = id;
-            Name = name;
-            ZipCode = zipCode;
-            Cuisine = cuisine;
+            return fileManager.GetAll();
         }
-
-
     }
 }
