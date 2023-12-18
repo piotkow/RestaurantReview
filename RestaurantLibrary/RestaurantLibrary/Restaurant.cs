8395aecab9c528c7a9a819adb075dc6f8230e3d0
Diana Grzelak <diana-teodora.grzelak@sdworx.com>
2023-12-18 14:50:47 +0100

    add the seedRestaurants method

--- RestaurantLibrary/RestaurantLibrary/Restaurant.cs
+++ RestaurantLibrary/RestaurantLibrary/Restaurant.cs
@@ -32,6 +32,26 @@
             Cuisine = cuisine;
         }
 
+        public void seedRestaurants()
+        {
+            List<Restaurant> restaurants = new List<Restaurant>()
+            {
+                new Restaurant(1,"Pierogi Place","80-001",Cuisine.Polish),
+                new Restaurant(2,"Tacos El Sol","70-003",Cuisine.Mexican),
+                new Restaurant(3,"Best Kebab","53-089",Cuisine.Turkish),
+                new Restaurant(4,"Bella Napoli Pizzeria","50-003",Cuisine.Italian),
+                new Restaurant(5,"Thai Orchid","40-456",Cuisine.Thai),
+                new Restaurant(6,"Delicious Sarmale","10-568",Cuisine.Romanian),
+                new Restaurant(7,"Taj Tandoori","93-893",Cuisine.Indian),
+                new Restaurant(8,"Dragon's Delight","38-002",Cuisine.Chinese),
+                new Restaurant(9,"Sakura Sushi","30-709",Cuisine.Japanese),
+                new Restaurant(10,"Seoul Savory","11-231",Cuisine.Korean),
+                new Restaurant(11,"Aegean Eats","46-078",Cuisine.Greek)
+
+            };
+        }
+
+
 
     }
 }
