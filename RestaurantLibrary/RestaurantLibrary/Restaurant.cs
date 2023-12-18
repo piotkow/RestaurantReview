b9814d511b6755dce8f299b8ea274a7e6c6b9072
Diana Grzelak <diana-teodora.grzelak@sdworx.com>
2023-12-18 14:10:12 +0100

    prepare the Restaurant class

--- RestaurantLibrary/RestaurantLibrary/Restaurant.cs
+++ RestaurantLibrary/RestaurantLibrary/Restaurant.cs
@@ -1,15 +1,37 @@
 ﻿namespace RestaurantLibrary
 {
+    public enum Cuisine
+    {
+        Chinese,
+        Japanese,
+        Korean,
+        Thai,
+        Italian,
+        Indian,
+        Mexican,
+        Polish,
+        Lebanese,
+        Romanian,
+        Greek,
+        Turkish,
+        Other
+    }
     public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string ZipCode { get; set; }
-        public string Cuisine { get; set; }
+        public Cuisine Cuisine { get; set; }
 
-        public List<Restaurant> GetDetails(FileManager<Restaurant> fileManager)
+
+        public Restaurant(int id, string name, string zipCode, Cuisine cuisine)
         {
-            return fileManager.GetAll();
+            Id = id;
+            Name = name;
+            ZipCode = zipCode;
+            Cuisine = cuisine;
         }
+
+
     }
 }
